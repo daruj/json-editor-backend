18eaa2dfe74bbdbd018111830a0f8436d1002d42
Damian Aruj <aruj.damian@gmail.com>
2023-10-28 12:51:31 +0300

    allow cors from localhost:3000

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import 'module-alias/register'
 
 import express from 'express'
 import bodyParser from 'body-parser'
-
+import cors from 'cors'
 import { wsServer } from '@src/websocket'
 import { router } from '@src/modules/json/json.routes'
 import { config } from '@src/config'
 
 const app = express()
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+    }),
+)
 
 // Middleware to parse JSON in the request body
 app.use(bodyParser.json())
